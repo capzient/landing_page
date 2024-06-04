43675bfbd2824517cc1877228551cb302f574442
Shahil <sahilaryan9211@gmail.com>
2024-06-04 23:03:28 +0530

    added animations

--- src/components/sub/HeroContent.jsx
+++ src/components/sub/HeroContent.jsx
@@ -3,33 +3,58 @@
 import React from "react";
 import { motion } from "framer-motion";
 import WithRedRect from "../utils/withRedRect";
+import {
+  MobileLinkVars,
+  ContainerVars,
+} from "./../utils/animations/TextAnimation";
 
 const HeroContent = () => {
   return (
     <motion.div
-      initial="hidden"
-      animate="visible"
+      variants={ContainerVars}
+      initial="initial"
+      animate="animate"
       className="flex flex-col items-center justify-center px-3 md:px-20 mt-40 w-full z-[20]"
     >
-      <motion.div className="h-full w-full flex flex-col  justify-evenly text-start">
-        <motion.div className="tracking-wider  md:mt-6 text-4xl md:text-9xl font-medium text-white w-full roboto items-center">
-          <div>
-            WE HELP
-          </div>
-          <div>
-            LEADERS BUILD
-          </div>
-          <div className="font-extrabold text-6xl md:text-[140px] ">
-            <WithRedRect>
-              TOMORROW
-            </WithRedRect>
-          </div>
-        </motion.div>
-        <motion.div className="pt-[470px] md:pt-32 flex flex-row justify-between">
-          <motion.div className="text-white w-8 md:w-auto text-xl md:text-4xl roboto text-start  flex items-center">
-            <WithRedRect>
-              Capabilities that fuel the future
-            </WithRedRect>
+      <motion.div className="h-full w-full flex flex-col justify-evenly text-start">
+        <div className="overflow-hidden">
+          <motion.div
+            variants={MobileLinkVars}
+            initial="initial"
+            animate="open"
+            className="overflow-hidden tracking-wider md:mt-6 text-4xl md:text-9xl font-medium text-white w-full roboto items-center"
+          ></motion.div>
+          <motion.div
+            variants={MobileLinkVars}
+            initial="initial"
+            animate="open"
+            className="overflow-hidden tracking-wider md:mt-6 text-4xl md:text-9xl font-medium text-white w-full roboto items-center"
+          >
+            <div className="overflow-hidden">
+              <motion.div variants={MobileLinkVars}>WE HELP</motion.div>
+            </div>
+            <div className="overflow-hidden">
+              <motion.div variants={MobileLinkVars}>LEADERS BUILD</motion.div>
+            </div>
+            <motion.div
+              variants={MobileLinkVars}
+              className="text-6xl md:text-[140px] font-extrabold"
+            >
+              <WithRedRect>TOMORROW</WithRedRect>
+            </motion.div>
+          </motion.div>
+        </div>
+        <motion.div
+          variants={MobileLinkVars}
+          initial="initial"
+          animate="open"
+          className="pt-[470px] md:pt-32 flex flex-row justify-between overflow-hidden"
+        >
+          <motion.div
+            variants={MobileLinkVars}
+            className="text-white w-8 md:w-auto text-xl md:text-4xl roboto text-start  flex items-center"
+          >
+            <WithRedRect>Capabilities that fuel the future</WithRedRect>
           </motion.div>
           <button className="text-white border text-sm md:text-lg border-white px-5 py-[2px] md:px-16 md:py-6 rounded-md hover:bg-white hover:text-black">
             Power your project now
@@ -41,3 +66,48 @@ const HeroContent = () => {
 };
 
 export default HeroContent;
+
+// "use client";
+
+// import React from "react";
+// import { motion } from "framer-motion";
+// import WithRedRect from "../utils/withRedRect";
+// import MobileLinkVars from './../utils/animations/TextAnimation';
+
+// const HeroContent = () => {
+//   return (
+//     <motion.div
+//       initial="hidden"
+//       animate="visible"
+//       className="flex flex-col items-center justify-center px-3 md:px-20 mt-40 w-full z-[20]"
+//     >
+//       <motion.div className="h-full w-full flex flex-col  justify-evenly text-start">
+//         <motion.div className="tracking-wider  md:mt-6 text-4xl md:text-9xl font-medium text-white w-full roboto items-center">
+//           <div>
+//             WE HELP
+//           </div>
+//           <div>
+//             LEADERS BUILD
+//           </div>
+//           <div className="font-extrabold text-6xl md:text-[140px] ">
+//             <WithRedRect>
+//               TOMORROW
+//             </WithRedRect>
+//           </div>
+//         </motion.div>
+//         <motion.div className="pt-[470px] md:pt-32 flex flex-row justify-between">
+//           <motion.div className="text-white w-8 md:w-auto text-xl md:text-4xl roboto text-start  flex items-center">
+//             <WithRedRect>
+//               Capabilities that fuel the future
+//             </WithRedRect>
+//           </motion.div>
+//           <button className="text-white border text-sm md:text-lg border-white px-5 py-[2px] md:px-16 md:py-6 rounded-md hover:bg-white hover:text-black">
+//             Power your project now
+//           </button>
+//         </motion.div>
+//       </motion.div>
+//     </motion.div>
+//   );
+// };
+
+// export default HeroContent;
---
+++ src/components/utils/animations/TextAnimation.jsx
@@ -0,0 +1,63 @@
+const MobileLinkVars = {
+  initial: {
+    y: "30vh",
+    transition: {
+      duration: 0.7,
+      ease: [0.37, 0, 0.63, 1],
+    },
+  },
+  open: {
+    y: 0,
+    transition: {
+      ease: [0, 0.55, 0.45, 1],
+      duration: 0.7,
+      delayChildren: 0.1,
+      staggerChildren: 0.3, // Delay between each letter
+    },
+  },
+  letter: {
+    initial: {
+      opacity: 0,
+      y: 20,
+    },
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  },
+};
+
+const ContainerVars = {
+  initial: {
+    transition: {
+      staggerChildren: 0.09,
+    },
+  },
+  open: {
+    transition: {
+      staggerChildren: 0.09,
+    },
+  },
+};
+
+export { MobileLinkVars, ContainerVars };
+
+// const MobileLinkVars = {
+//   initial: {
+//     y: "30vh",
+//     transition: {
+//       duration: 0.7,
+//       ease: [0.37, 0, 0.63, 1],
+//     },
+//   },
+//   open: {
+//     y: 0,
+//     transition: {
+//       ease: [0, 0.55, 0.45, 1],
+//       duration: 0.7,
+//     },
+//   },
+// };
