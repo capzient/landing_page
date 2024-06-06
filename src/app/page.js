24b0401c21bd17d610bb3ae661d862842e44efa9
Ayush Chaturvedi <2200520100019@ietlucknow.ac.in>
2024-06-06 18:43:04 +0530

    (feat):added footer component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "../components/layout/footer/Footer";
 import Hero from "../components/main/Hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="h-full w-full">
       <div>
         <Hero />
+        <Footer />
       </div>
     </main>
   );
