5cc8d2ccfd09d2a3321ab464c04d5497aea920d5
Ayush Chaturvedi <2200520100019@ietlucknow.ac.in>
2024-06-07 13:40:38 +0530

    improved footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -13,8 +13,8 @@ export default function Footer() {
             <ul>
               <li className="font-bold mb-8">Company</li>
               <li>About Us</li>
-              <li>Contact Us</li>
-              <li>Location</li>
+              <li className="my-2">Contact Us</li>
+              <li className="mb-2">Location</li>
               <li>Careers</li>
             </ul>
           </div>
@@ -22,7 +22,7 @@ export default function Footer() {
             <ul>
               <li className="font-bold mb-8">Legal and Policy</li>
               <li>Privacy Statement</li>
-              <li>Terms and Condition</li>
+              <li className="my-2">Terms and Condition</li>
               <li>Cookies Policy</li>
             </ul>
           </div>
@@ -30,9 +30,9 @@ export default function Footer() {
             <ul>
               <li className="font-bold mb-8">Services</li>
               <li>Blockchain</li>
-              <li>Cloud Computing</li>
+              <li className="my-2">Cloud Computing</li>
               <li>Web 3.0</li>
-              <li>Machine Learning</li>
+              <li className="my-2">Machine Learning</li>
               <li>Cybersecurity</li>
             </ul>
           </div>
@@ -40,24 +40,27 @@ export default function Footer() {
             <ul>
               <li className="font-bold mb-8">FOLLOW US</li>
 
-              <li className="grid grid-cols-3 gap-0">
-                <span>
+              <li className="flex justify-start">
+                <span className="relative">
+                  <div className="rounded-full h-8 w-8 bg-gray-400 absolute -top-1.5 -left-2 opacity-50"></div>
                   <ion-icon name="logo-facebook"></ion-icon>{" "}
                 </span>
 
-                <span>
+                <span className="mx-8 relative">
+                  <div className="rounded-full h-8 w-8 bg-gray-400 absolute -top-1.5 -left-2 opacity-50"></div>
                   <ion-icon name="logo-xing"></ion-icon>{" "}
                 </span>
 
-                <span>
+                <span className="mx-2 relative">
+                  <div className="rounded-full h-8 w-8 bg-gray-400 absolute -top-1.5 -left-2 opacity-50"></div>
                   <ion-icon name="logo-instagram"></ion-icon>{" "}
                 </span>
               </li>
             </ul>
           </div>
         </div>
-        <div className="grid grid-cols-3 my-10 p-4 pt-6 pb-6">
-          <div>
+        <div className="grid grid-cols-3 my-10 p-4 pt-6 pb-6 mb-0">
+          <div className="mx-16">
             <ion-icon name="business-outline"></ion-icon> 2024 Company
           </div>
           <div className="col-start-3">
@@ -67,7 +70,7 @@ export default function Footer() {
             <span className="mx-2">
               <ion-icon name="chevron-down-outline"></ion-icon>
             </span>
-            <span className="mx-16">
+            <span className="mx-8">
               <ion-icon name="arrow-up-outline"></ion-icon>
             </span>
           </div>
