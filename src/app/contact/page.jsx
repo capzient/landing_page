a1e3e264230cae36c989d6e254e31d6779cafb7e
Shahil <sahilaryan9211@gmail.com>
2024-06-10 04:07:27 +0530

    fix:contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,7 @@
 import Contact from "@/src/components/contact";
 export default function contact() {
   return (
-    <div className="pt-36 pb-52 bg-neutral-950">
+    <div>
       <Contact />
     </div>
   );
