30ca329b07e19e4264a9729677441474553718a9
Wolmer <contact@wolmer.me>
2024-06-10 20:20:56 +0530

    fix: add breakpoint style for small screens

--- src/components/main/welcome/index.jsx
+++ src/components/main/welcome/index.jsx
@@ -34,7 +34,7 @@ const Welcome = () => {
           </div>
         </div>
       </div>
-      <div className="bg-[#111111] text-white p-[30px]">
+      {/* <div className="bg-[#111111] text-white p-[30px]">
         {bullets.map((aBullet, idx) => (
           <div key={idx} className="flex flex-wrap pb-[100px]">
             <div className="w-[200px]">
@@ -52,34 +52,44 @@ const Welcome = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
 
 
       <div className="bg-black w-full text-white text-8xl font-semibold font-['Inter'] uppercase h-full flex flex-col gap-20 justify-center items-center px-10 py-10">
-      <div className="space-y-10 w-full">
-        <p className="uppercase flex ">Unleash tomorrow&apos;s</p>
-        <div className="uppercase flex justify-end items-end">
+      <div className="space-y-10 w-full flex flex-col gap-10 py-10">
+        <p className="uppercase flex md:text-[96px] text-3xl">Unleash tomorrow&apos;s</p>
+        <div className="uppercase flex justify-end items-end md:text-[96px] text-3xl">
           <WithRedRect>Tech Today</WithRedRect>
         </div>
       </div>
       <Swiper
         spaceBetween={50}
-        slidesPerView={2}
+        slidesPerView={1}
+         breakpoints={{
+        // >= 768
+        768: {
+          slidesPerView: 2,
+        },
+        //  < 768
+        0: {
+          slidesPerView: 1,
+        },
+      }}
        
         className="w-full h-full flex justify-center items-center gap-10"
       >
         {carouselData.map((item, index) => (
           <SwiperSlide key={index}>
-            <div className="relative w-[609px] h-[582px] bg-gradient-to-b from-black to-black rounded-2xl border border-white/20">
-              <div className=" flex-col w-full h-full justify-start items-start inline-flex gap-[52px]  px-10">
-                <div className="w-[127.77px] h-[544.38px] left-[50%] rotate-[-55.41deg] blur-3xl bg-gradient-to-l from-red-700 to-orange-950 rounded-full absolute opacity-50" />
-                <div className="text-[40px] text-[#F9EFEC]">
+            <div className="relative  w-full md:w-[609px] h-[582px] bg-gradient-to-b from-black to-black rounded-2xl border border-white/20 px-10">
+              <div className=" flex-col w-full h-full justify-start items-start inline-flex gap-10 md:gap-[52px]">
+                <div className="md:w-[127.77px] w-full h-[582px] md:h-[544.38px] left-[50%] rotate-[-55.41deg] blur-3xl bg-gradient-to-l from-red-700 to-orange-950 rounded-full absolute opacity-50" />
+                <div className="text-2xl md:text-4xl text-[#F9EFEC] ">
                   <span className="text-[#F51101] ">&quot;</span>{item.heading}<span className="text-[#F51101]">&rdquo;</span>
                 </div>
-                <div className="text-[24px] text-[#A3A3A3]">
+                <div className=" text-xs md:text-2xl text-[#A3A3A3]">
                   {item.description}
                 </div>
-                <div className="gap-[65px] text-sm flex justify-end items-end">
+                <div className="gap-16 text-sm flex justify-center items-center">
                   <div className="icon">
                     <Image
                       width={50}
@@ -89,7 +99,7 @@ const Welcome = () => {
                       src={`/assets/images/welcome/graphic/${item.image}`}
                     />
                   </div>
-                  <div className="text-[28px]">{item.subhead}</div>
+                  <div className="text-sm md:text-2xl">{item.subhead}</div>
                 </div>
               </div>
             </div>
