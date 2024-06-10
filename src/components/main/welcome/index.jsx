3d1f111593d3e6fd31e4731e2d10b706f95bf061
Wolmer <contact@wolmer.me>
2024-06-10 20:05:12 +0530

    feat: add Swiper js - Add Carousel Section

--- src/components/main/welcome/index.jsx
+++ src/components/main/welcome/index.jsx
@@ -1,7 +1,12 @@
+'use client'
 import WithRedRect from "../../utils/withRedRect";
 import WithRedLeftBar from "../../utils/withRedLeftBar";
 import Image from "next/image";
-import { bullets } from "./data";
+import { bullets, carouselData } from "./data";
+import { InView } from "react-intersection-observer";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
 const Welcome = () => {
   return (
     <div>
@@ -48,6 +53,55 @@ const Welcome = () => {
           </div>
         ))}
       </div>
+
+
+      <div className="bg-black w-full text-white text-8xl font-semibold font-['Inter'] uppercase h-full flex flex-col gap-20 justify-center items-center px-10 py-10">
+      <div className="space-y-10 w-full">
+        <p className="uppercase flex ">Unleash tomorrow&apos;s</p>
+        <div className="uppercase flex justify-end items-end">
+          <WithRedRect>Tech Today</WithRedRect>
+        </div>
+      </div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={2}
+       
+        className="w-full h-full flex justify-center items-center gap-10"
+      >
+        {carouselData.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div className="relative w-[609px] h-[582px] bg-gradient-to-b from-black to-black rounded-2xl border border-white/20">
+              <div className=" flex-col w-full h-full justify-start items-start inline-flex gap-[52px]  px-10">
+                <div className="w-[127.77px] h-[544.38px] left-[50%] rotate-[-55.41deg] blur-3xl bg-gradient-to-l from-red-700 to-orange-950 rounded-full absolute opacity-50" />
+                <div className="text-[40px] text-[#F9EFEC]">
+                  <span className="text-[#F51101] ">&quot;</span>{item.heading}<span className="text-[#F51101]">&rdquo;</span>
+                </div>
+                <div className="text-[24px] text-[#A3A3A3]">
+                  {item.description}
+                </div>
+                <div className="gap-[65px] text-sm flex justify-end items-end">
+                  <div className="icon">
+                    <Image
+                      width={50}
+                      height={50}
+                      className="w-28"
+                      alt="graphic"
+                      src={`/assets/images/welcome/graphic/${item.image}`}
+                    />
+                  </div>
+                  <div className="text-[28px]">{item.subhead}</div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+
+
+
+      <div className="bg-[#111111] p-[30px] "></div>
+      <div className="flex bg-cover p-[30px] bg-center bg-[url('/assets/images/welcome/bg_2.svg')]"></div>
       <div className="bg-[#111111] p-[30px] ">
         <div className="flex bg-cover p-[30px] bg-center bg-[url('/assets/images/welcome/bg_2.svg')]">
           <div className="w-[50%]"></div>
