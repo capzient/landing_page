7fa29ba9fe05abc5eeb6a631c6f699274e0fa956
Wolmer <contact@wolmer.me>
2024-06-11 14:25:12 +0530

    feat: Add homepage other sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Footer from '@/components/layout/footer/Footer';
 import Careers from '@/components/main/careers';
+import EmergingTech from '@/components/main/emergingtech';
 import Hero from '@/components/main/hero';
+import NewsLetter from '@/components/main/newsletter';
 import Welcome from '@/components/main/welcome';
 
 export default function Home() {
@@ -8,6 +10,8 @@ export default function Home() {
     <main className="h-full w-full">
       <Hero />
       <Welcome />
+      <EmergingTech />
+      <NewsLetter />
       <Careers />
       <Footer />
     </main>
