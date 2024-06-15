3d1d7e75d046c583a4d4f4e911bf3acb17c14f9c
AppleSeed <everlastingfriendship194@gmail.com>
2024-06-15 23:11:06 +0900

    fix : hover image & newsletter

--- src/app/(homepage)/components/NewsLetterSection.jsx
+++ src/app/(homepage)/components/NewsLetterSection.jsx
@@ -6,13 +6,52 @@ import { ArrowButton } from '@/components/common/atoms/arrowButton';
 import { Card } from '@/components/common/atoms/card';
 import { Container } from '@/components/common/atoms/container';
 import { ExtraContentContainer } from '@/components/common/organisms/extraContentContainer';
+import { HoverImage } from '@/utils/hoverImage';
+import { WithRedTopBar } from '@/utils/withRedTopBar';
 
 export const NewsLetterSection = () => {
   const [openExtra, setOpenExtra] = useState(false);
   return (
     <div className="bg-black p-[30px] text-[#B3B3B2] px-10 flex flex-col items-center justify-center ">
       <Container containerClassName="p-3 rounded-2xl border h-full w-full flex flex-col gap-[15px]">
-        <ExtraContentContainer open={openExtra} setOpen={setOpenExtra} extraCpn={<div>extra contents here</div>}>
+        <ExtraContentContainer
+          open={openExtra}
+          setOpen={setOpenExtra}
+          extraCpn={
+            <div className="flex gap-[50px] justify-center pt-[150px] items-center h-full p-[50px]">
+              <div className="w-[400px]">
+                <WithRedTopBar width={100}>
+                  <HoverImage
+                    title={'sample title'}
+                    description="Lorem ipsum dolor sit amet consectetur. Ac mi vestibulum mi aliquam. Ultricies amet cursus sit mauris dui ut leo. Luctus consequat aliquet faucibus auctor suspendisse augue."
+                    className="flex items-center h-[600px] justify-center gap-2 relative  w-full"
+                    url={'/assets/images/welcome/graphic/newsletter1.svg'}
+                  ></HoverImage>{' '}
+                </WithRedTopBar>
+              </div>
+              <div className="w-[400px]">
+                <WithRedTopBar width={100}>
+                  <HoverImage
+                    title={'sample title'}
+                    description="Lorem ipsum dolor sit amet consectetur. Ac mi vestibulum mi aliquam. Ultricies amet cursus sit mauris dui ut leo. Luctus consequat aliquet faucibus auctor suspendisse augue."
+                    className="flex items-center h-[600px] justify-center gap-2 relative  w-full"
+                    url={'/assets/images/welcome/graphic/newsletter1.svg'}
+                  ></HoverImage>{' '}
+                </WithRedTopBar>
+              </div>
+              <div className="w-[400px]">
+                <WithRedTopBar width={100}>
+                  <HoverImage
+                    title={'sample title'}
+                    description="Lorem ipsum dolor sit amet consectetur. Ac mi vestibulum mi aliquam. Ultricies amet cursus sit mauris dui ut leo. Luctus consequat aliquet faucibus auctor suspendisse augue."
+                    className="flex items-center h-[600px] justify-center gap-2 relative  w-full"
+                    url={'/assets/images/welcome/graphic/newsletter1.svg'}
+                  ></HoverImage>{' '}
+                </WithRedTopBar>
+              </div>
+            </div>
+          }
+        >
           <Card bodyClassName="w-full p-5 flex justify-between items-center bg-[#191919] rounded-2xl">
             <p className="text-[#F9EFEC] text-[40px] p-[20px]">NEWS LETTER</p>
             <ArrowButton
@@ -28,14 +67,11 @@ export const NewsLetterSection = () => {
 
         <div className="grid grid-cols-3 gap-[15px] grid-rows-2 h-full w-full">
           <div className="animation-element appear w-full flex flex-col bg-[#191919] rounded-2xl h-full">
-            <div className="flex items-center h-[400px] justify-center gap-2 relative  w-full">
-              <Image
-                src="/assets/images/welcome/graphic/newsletter1.svg"
-                fill
-                className="object-cover rounded-t-2xl"
-                alt=""
-              />
-            </div>
+            <HoverImage
+              description="Lorem ipsum dolor sit amet consectetur. Ac mi vestibulum mi aliquam. Ultricies amet cursus sit mauris dui ut leo. Luctus consequat aliquet faucibus auctor suspendisse augue."
+              className="flex items-center h-[400px] rounded-t-2xl justify-center gap-2 relative  w-full"
+              url={'/assets/images/welcome/graphic/newsletter1.svg'}
+            ></HoverImage>
             <div className="h-[30%] w-full p-5 flex justify-between items-center">
               <p className="text-[#F9EFEC] text-[25px]">Lorem ipsum dolor sit amet consectetur.</p>
               <ArrowButton
@@ -93,14 +129,12 @@ export const NewsLetterSection = () => {
           </div>
           {/*  */}
           <div className="animation-element appear w-full flex flex-col bg-[#191919] rounded-2xl h-full col-span-2">
-            <div className="flex items-center h-[400px] justify-center gap-2 relative  w-full">
-              <Image
-                src="/assets/images/welcome/graphic/newsletter1.svg"
-                fill
-                className="object-cover rounded-t-2xl"
-                alt=""
-              />
-            </div>
+            <HoverImage
+              className="flex items-center h-[400px] rounded-t-2xl justify-center gap-2 relative  w-full"
+              description="Lorem ipsum dolor sit amet consectetur. Ac mi vestibulum mi aliquam. Ultricies amet cursus sit mauris dui ut leo. Luctus consequat aliquet faucibus auctor suspendisse augue."
+              url={'/assets/images/welcome/graphic/newsletter1.svg'}
+            ></HoverImage>
+
             <div className="h-[30%] w-full p-5 flex justify-between items-center">
               <p className="text-[#F9EFEC] text-[25px]">Lorem ipsum dolor sit amet consectetur.</p>{' '}
               <ArrowButton
@@ -114,14 +148,11 @@ export const NewsLetterSection = () => {
           </div>
           {/*  */}
           <div className="animation-element appear w-full flex flex-col bg-[#191919] rounded-2xl h-full">
-            <div className="flex items-center justify-center h-[400px]  gap-2 relative  w-full">
-              <Image
-                src="/assets/images/welcome/graphic/newsletter2.svg"
-                fill
-                className="object-cover rounded-t-2xl"
-                alt=""
-              />
-            </div>
+            <HoverImage
+              className="flex items-center h-[400px] rounded-t-2xl justify-center gap-2 relative  w-full"
+              description="Lorem ipsum dolor sit amet consectetur. Ac mi vestibulum mi aliquam. Ultricies amet cursus sit mauris dui ut leo. Luctus consequat aliquet faucibus auctor suspendisse augue."
+              url={'/assets/images/welcome/graphic/newsletter2.svg'}
+            ></HoverImage>
             <div className="h-[30%] w-full p-5 flex justify-between items-center">
               <p className="text-[#F9EFEC] text-[25px]">Lorem ipsum dolor sit amet consectetur.</p>{' '}
               <ArrowButton
