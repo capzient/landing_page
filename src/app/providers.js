d296ebe116dcff47ca9acded10768eaa15820a19
AppleSeed <everlastingfriendship194@gmail.com>
2024-06-17 21:55:57 +0900

    fix : conflict resolve

--- src/app/providers.js
+++ src/app/providers.js
@@ -6,7 +6,6 @@ import { SmoothScroll } from '@/utils/smoothScroll';
 
 export const Providers = ({ children }) => (
   <AnimatePresence>
-    <CursorFollower />
     <SmoothScroll>{children}</SmoothScroll>
   </AnimatePresence>
 );
