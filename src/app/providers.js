2050cd48d37ef9a9df8b69486ebcaeeaad67f4ef
Wolmer <contact@wolmer.me>
2024-06-17 14:48:16 +0530

    fix: remove cursorFollower

--- src/app/providers.js
+++ src/app/providers.js
@@ -2,12 +2,10 @@
 import { AnimatePresence } from 'framer-motion';
 import React from 'react';
 
-import { CursorFollower } from '@/utils/cursorFollower';
 import { SmoothScroll } from '@/utils/smoothScroll';
 
 export const Providers = ({ children }) => (
   <AnimatePresence>
-    <CursorFollower />
     <SmoothScroll>{children}</SmoothScroll>
   </AnimatePresence>
 );
