04cc058669e68abca5312b3179d857d944da97dc
Wolmer <contact@wolmer.me>
2024-06-19 23:20:53 +0530

    deploy: try1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 // const withSvgr = require('@svgr/webpack');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   // this is webpack configuration to allow svgr
   webpack(config) {
     config.module.rules.push({
