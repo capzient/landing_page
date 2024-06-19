ceae0beb658551be1b4ee4f47e3f35a10a9c5467
Wolmer <contact@wolmer.me>
2024-06-19 18:43:00 +0530

    fix; welcome section red dot

--- src/app/(homepage)/components/WelcomeSection.jsx
+++ src/app/(homepage)/components/WelcomeSection.jsx
@@ -72,12 +72,11 @@ export const WelcomeSection = () => {
 
         <WavyBackground>
           <div className="max-w-full mx-auto pb-40">
-            <WithRedRect>
-              <div className="z-50 inset-0 flex items-center justify-center text-3xl text-white font-bold px-4 pointer-events-none text-center md:text-4xl lg:text-9xl">
-                We Re-
-                <FlipWords words={words} />
-              </div>
-            </WithRedRect>
+            <div className="z-50 inset-0 flex items-center justify-center text-3xl text-white font-bold px-4 pointer-events-none text-center md:text-4xl lg:text-9xl">
+              We Re-
+              <FlipWords words={words} />
+              <span className={`text-red-500 dot`}>.</span>
+            </div>
           </div>
         </WavyBackground>
 
