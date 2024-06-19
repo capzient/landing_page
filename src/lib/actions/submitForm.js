1ed5a461d29a01fc2de7f7ce5d2734e2d5e8e67b
Wolmer <contact@wolmer.me>
2024-06-19 16:05:43 +0530

    feat: Send contact form data to google forms

---
+++ src/lib/actions/submitForm.js
@@ -0,0 +1,17 @@
+import { stringifyData } from '@/utils/stringifyUrl';
+
+export const submitForm = async (data) => {
+  const stringifiedData = await stringifyData(data);
+
+  console.log(stringifiedData);
+
+  const response = await fetch(process.env.GOOGLE_FORM_URL + stringifiedData, {
+    method: 'POST',
+    cache: 'no-cache',
+  });
+  if (response.status !== 200) {
+    return false;
+  } else {
+    return true;
+  }
+};
